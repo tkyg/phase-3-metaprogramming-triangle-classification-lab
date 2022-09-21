e718378b65b0727546233d412d2cc61839ea17fe
tkyg <80114433+tkyg@users.noreply.github.com>
2022-09-21 16:44:07 -0400

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sum1 = self.side1 + self.side2
+    sum2 = self.side2 + self.side3
+    sum3 = self.side1 + self.side3
+
+  if (self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0 || sum1 <= self.side3 || sum2 <= self.side1 || sum3 <= self.side2)
+      raise TriangleError
+    elsif (self.side1 == self.side2 && self.side2 == self.side3)
+      return :equilateral
+    elsif (self.side1 == self.side2 || self.side1 == self.side3 || self.side2 == self.side3)
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "There are not valid sizes"
+    end
+  end
+
 end
